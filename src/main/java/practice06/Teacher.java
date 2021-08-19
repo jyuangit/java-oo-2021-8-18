e1f72509a0034c1f678203f8f134fa0de073bdfd
jyuangit <jiyuan233@qq.com>
2021-08-19 14:07:44 +0800

    person6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends  Person{
+    private Integer klass;
+
+    public Teacher(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, Integer age) {
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String words = super.introduce() + " I am a Teacher.";
+        words +=
+                this.klass != null ? String.format(" I teach Class %d.", this.klass) : " I teach No Class.";
+        return words;
+    }
 }
