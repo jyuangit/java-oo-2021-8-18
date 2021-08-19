71c2a8275ddd73557ffda01b6f22d32b7d63b9ca
jyuangit <jiyuan233@qq.com>
2021-08-19 14:01:13 +0800

    person2

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,33 @@
 package practice02;
 
 public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Person(String name, Integer age) {
+
+        this.name = name;
+        this.age = age;
+    }
+
+    private Integer age;
+
+    public String introduce(){
+        return String.format("My name is %s. I am %d years old.", this.name, this.age);
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,25 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    private Integer klass;
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    public Student(String name, Integer age, Integer klass) {
+
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("I am a Student. I am at Class %d.",this.klass );
+    }
+
 }
