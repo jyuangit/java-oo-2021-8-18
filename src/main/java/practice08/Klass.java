6916a2bf10614f2b71e65bc2c917f3c9252f9ab1
jyuangit <jiyuan233@qq.com>
2021-08-19 14:11:52 +0800

    person8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,35 @@
 package practice08;
 
 public class Klass {
+    private Integer number;
+
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        this.setLeader(student);
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,36 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends  Person{
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(Integer id,String name, Integer age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public String introduceWith(Student student){
+        String words = String.format("My name is %s. I am %d years old. I am a Teacher.", this.getName(), this.getAge());
+        words += student.getKlass().equals(this.klass)? String.format(" I teach %s.",student.getName()) : String.format(" I don't teach %s.",student.getName());
+        return words;
+    }
+
+    @Override
+    public String introduce() {
+        String words = super.introduce() + " I am a Teacher.";
+        words += this.klass != null ? String.format(" I teach Class %d.", this.klass.getNumber())
+                : " I teach No Class.";
+        return words;
+    }
 }
